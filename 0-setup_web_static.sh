dd2f0d0ccc3d444ab7d2edbf4092045450a8519c
Justin4587 <Thurman@atlasok.com>
2021-04-15 18:22:36 -0500

    Asdf

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -9,6 +9,5 @@ sudo ln -sf /data/web_static/releases/test/ data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data/
 sudo service nginx stop
 location="/etc/nginx/sites-enabled/default"
-sudo sed -i '20i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' $location
-sudo rm /etc/sites-enabled/default~
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' $location
 sudo service nginx restart
