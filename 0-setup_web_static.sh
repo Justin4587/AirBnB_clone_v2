fcc5f2ead936bd37a5eab5f6c18c9753fb1ffd59
Justin4587 <Thurman@atlasok.com>
2021-04-15 21:55:35 -0500

    Asdf

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,10 +4,10 @@
 sudo apt-get -y update
 sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-sudo echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test/ data/web_static/current
+sudo echo "My Existance is Miserable" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data/
 sudo service nginx stop
 location="/etc/nginx/sites-enabled/default"
-sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' $location
+sudo sed -i '43i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' $location
 sudo service nginx restart
