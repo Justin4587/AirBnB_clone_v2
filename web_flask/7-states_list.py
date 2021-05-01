923dcfae48a4228a3c26c17bf5d9adae5f5053af
Justin4587 <Thurman@atlasok.com>
2021-05-01 18:33:34 -0500

    asdf

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Wish I had a full Flask"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.teardown_appcontext
+def teardown(self):
+    """ hello """
+    storage.close()
+
+
+@app.route('/states_list')
+def state_list():
+    st8 = storage.all(State).values()
+    return render_template('7-states_list.html', states=st8)
