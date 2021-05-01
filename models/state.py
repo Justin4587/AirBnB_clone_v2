0e2f7d66ccc7f051d35bed6f05fa79908ce932b0
Justin4587 <Thurman@atlasok.com>
2021-05-01 16:18:41 -0500

    asdf

--- models/state.py
+++ models/state.py
@@ -1,26 +1,29 @@
+
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Table
-from sqlalchemy.orm import relationship
-from models.city import City
 from os import getenv
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 import models
+
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        cities = relationship("City", cascade="all, delete", backref="state")
-    else:
+    __tablename__ = "states"
+
+    if getenv("HBNB_TYPE_STORAGE") != "db":
+        name = ""
+
         @property
         def cities(self):
-            """returns list of related cities based on state id"""
-            from models import storage
-            city_all = storage.all(City)
-            city_related = []
-            for key, val in city_all.items():
-                if city_all[key].state_id == self.id:
-                    city_related.append(val)
-            return city_related
+            """List of cities in that state"""
+            result = []
+            for i in models.storage.all("City").values():
+                if i.state_id == self.id:
+                    result.append(city)
+            return result
+    else:
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state",
+                              cascade="all, delete, delete-orphan")
