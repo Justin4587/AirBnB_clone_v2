054a5b236569d364a3348619d02d352b06b2895e
Justin4587 <Thurman@atlasok.com>
2021-05-01 16:59:56 -0500

    asdf

--- models/state.py
+++ models/state.py
@@ -4,7 +4,6 @@ from models.base_model import BaseModel, Base
 from os import getenv
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from models.city import City
 import models
 
 
