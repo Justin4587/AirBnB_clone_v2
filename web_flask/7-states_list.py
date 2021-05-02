08a53a71eece473f540c644c910c3a14ba9a3e2e
Justin4587 <Thurman@atlasok.com>
2021-05-01 19:42:35 -0500

    Asdf

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -3,7 +3,7 @@
 from flask import Flask, render_template
 from models import storage
 from models.state import State
-from operator import attrgetter
+
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
@@ -15,10 +15,9 @@ def teardown(self):
 
 
 @app.route('/states_list')
-def state_list():
-    states = storage.all(State).values()
-    sort_states = sorted(states, key=attrgetter('name'))
-    return render_template('7-states_list.html', states=sort_states)
+def states_list():
+    states = storage.all('State').values()
+    return render_template('7-states_list.html', states=states)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
